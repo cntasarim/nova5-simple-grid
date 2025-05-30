fa046d00d970708264c3a2ff511d5c5043ec31dd
Cemil Nayman <cntasarim@outlook.com.tr>
2025-05-30 20:27:03 +0300

    Update tool.js

--- src/resources/js/tool.js
+++ src/resources/js/tool.js
@@ -1,4 +1,5 @@
 Nova.booting((Vue, router, store) => {
+    // Form alanları için wrapper
     Vue.component('form-field-wrapper', {
         props: ['field'],
         computed: {
@@ -7,8 +8,21 @@ Nova.booting((Vue, router, store) => {
             }
         },
         template: `
-            <div :class="sizeClass">
+            <div :class="[sizeClass, 'px-2']">
                 <slot />
+            </div>
+        `
+    });
+
+    // Panel bileşeni için flex düzeni
+    Vue.component('panel', {
+        props: ['name', 'fields'],
+        template: `
+            <div class="mb-6">
+                <h3 class="text-lg font-semibold mb-2">{{ name }}</h3>
+                <div class="flex flex-wrap -mx-2">
+                    <slot />
+                </div>
             </div>
         `
     });
